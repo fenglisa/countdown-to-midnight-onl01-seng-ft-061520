705bbaf14716ccb1f363727707e79c253994a876
Lisa Feng <lisafeng10@gmail.com>
2020-06-16 14:09:08 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,17 +1,18 @@
 number = 20
-seconds = 1
+
 
 def countdown_with_sleep(number)
   while number > 0
-    sleep(1)
+    puts "#{number} SECOND(S)!"
     number -= 1
+    sleep(1)
   end
+  return "HAPPY NEW YEAR!"
 end
 
 def countdown(number)
   while number > 0
     puts "#{number} SECOND(S)!"
-    countdown_with_sleep(number)
     number -= 1
   end
   return "HAPPY NEW YEAR!"
